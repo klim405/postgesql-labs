7417502c17442bff506508409dbfb7972ec27f86
klim405 <klim405.develop@gmail.com>
2024-04-05 17:19:15 +0300

    fix: Доработан скрип и изменены тесты.

--- lab2-1/init.sql
+++ lab2-1/init.sql
@@ -2,7 +2,9 @@ DROP TABLE IF EXISTS test1;
 
 CREATE TABLE test1 (
     "'hello" integer,
-    "world''" integer
+    "''hello" integer,
+    "world''" integer,
+    """" integer
 );
 
-INSERT INTO test1 VALUES (1, 5), (1, 8);
+INSERT INTO test1 VALUES (1, 5, 12, 12), (1, 8, 12, 12);
--- lab2-1/main.sql
+++ lab2-1/main.sql
@@ -7,17 +7,30 @@ do $$
         col_info record;
     BEGIN
         table_cnt := count(distinct table_name) from (
-            SELECT table_name
-            FROM information_schema.columns
-            WHERE table_schema=schema_name and (column_name like E'%\'%' or column_name like '%"%')
+            select a.table_name
+            from information_schema.columns as a
+            JOIN information_schema.role_table_grants as b
+            ON a.table_schema = b.table_schema and a.table_name = b.table_name and b.privilege_type = 'UPDATE'
+            WHERE a.table_schema=schema_name and (a.column_name like E'%\'%' or a.column_name like '%"%')
             ) as res;
+
         FOR col_info IN
-            SELECT table_name, column_name
-            FROM information_schema.columns
-            WHERE table_schema=schema_name and (column_name like E'%\'%' or column_name like '%"%')
+            SELECT a.table_name, a.column_name
+            FROM information_schema.columns as a
+            JOIN information_schema.role_table_grants as b
+            ON a.table_schema = b.table_schema and a.table_name = b.table_name and b.privilege_type = 'UPDATE'
+            WHERE a.table_schema=schema_name and (a.column_name like E'%\'%' or a.column_name like '%"%')
         LOOP
             new_col_name := replace(col_info.column_name, E'\'', '');
-            new_col_name := replace(new_col_name, E'"', '');
+            new_col_name := replace(new_col_name, '"', '');
+            if length(new_col_name) = 0 then
+                new_col_name := 'kavichka';
+            end if;
+            WHILE
+                (select count(*) from information_schema.columns where table_schema=schema_name and table_name=col_info.table_name and column_name=new_col_name) > 0
+            LOOP
+                new_col_name := concat(new_col_name, '_renamed');
+            END LOOP;
             EXECUTE format(
                     'ALTER TABLE %I.%I RENAME COLUMN %I TO %I',
                     schema_name, col_info.table_name, col_info.column_name, new_col_name);
